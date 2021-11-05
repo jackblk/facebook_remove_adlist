5e7ef46af5220029fa1ff929a62af917bbc49201
Trung Le <jackblk4@gmail.com>
2021-11-05 14:55:54 +0700

    feat: get cookies manually - added email arg

--- get_cookies_manual.py
+++ get_cookies_manual.py
@@ -1,16 +1,25 @@
 """
 Generate token and cookies from app password
 """
+import click
 from getpass import getpass
 from api.fbook import FbAuth
 
-if __name__ == "__main__":
+
+@click.command()
+@click.option("--email", default=None, help="Email or username")
+def get_cookies(email):
     # get app password here
     # https://www.facebook.com/settings?tab=security&section=per_app_passwords
-    email_ = input("Enter email or username:")
+    if email is None:
+        email = input("Enter email or username: ")
     password_ = getpass()
-    auth = FbAuth(email_, password_)
+    auth = FbAuth(email, password_)
     env_str_cookie = f'FB_COOKIES="{auth.get_cookies_string()}"'
 
     with open(".env", "w", encoding="utf-8") as f:
         f.write(env_str_cookie)
+
+
+if __name__ == "__main__":
+    get_cookies()  # pylint: disable=no-value-for-parameter
