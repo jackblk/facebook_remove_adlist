3380907f94fa04933a46b6f160b25ee1647fc1e9
Trung Le <jackblk4@gmail.com>
2021-11-05 15:02:01 +0700

    fix: pylint error

--- get_cookies_manual.py
+++ get_cookies_manual.py
@@ -1,14 +1,19 @@
 """
 Generate token and cookies from app password
 """
-import click
 from getpass import getpass
+import click
 from api.fbook import FbAuth
 
 
 @click.command()
 @click.option("--email", default=None, help="Email or username")
 def get_cookies(email):
+    """Get cookies and export to .env file
+
+    Args:
+        email (string): email for username
+    """
     # get app password here
     # https://www.facebook.com/settings?tab=security&section=per_app_passwords
     if email is None:
@@ -17,8 +22,8 @@ def get_cookies(email):
     auth = FbAuth(email, password_)
     env_str_cookie = f'FB_COOKIES="{auth.get_cookies_string()}"'
 
-    with open(".env", "w", encoding="utf-8") as f:
-        f.write(env_str_cookie)
+    with open(".env", "w", encoding="utf-8") as file:
+        file.write(env_str_cookie)
 
 
 if __name__ == "__main__":
